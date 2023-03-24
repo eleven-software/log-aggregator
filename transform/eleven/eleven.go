39326d8f861d7c687287786e68b7931029563324
Ibaakee Ledum <ledumibaakee@gmail.com>
2023-03-24 14:06:33 +0100

    changed var name

--- transform/eleven/eleven.go
+++ transform/eleven/eleven.go
@@ -10,7 +10,7 @@ import (
 const (
 	EnvProduct   = "ELEVEN_PRODUCT"
 	EnvComponent = "ELEVEN_COMPONENT"
-	EnvLogFormat = "LOG_FORMAT"
+	EnvTimeFormat = "TIME_FORMAT"
 )
 
 func New() transform.Transformer {
@@ -18,16 +18,16 @@ func New() transform.Transformer {
 	product := os.Getenv(EnvProduct)
 	component := os.Getenv(EnvComponent)
 
-	logFormat := os.Getenv(EnvLogFormat)
-	if logFormat == "" {
-		logFormat = "2006-01-02T15:04:05.000"
+	timeFormat := os.Getenv(EnvTimeFormat)
+	if timeFormat == "" {
+		timeFormat = "2006-01-02T15:04:05.000Z"
 	}
 
 	return func(rec *types.Record) (*types.Record, error) {
 		rec.Fields["product"] = product
 		rec.Fields["component"] = component
 
-		formattedTime := rec.Time.Format(logFormat)
+		formattedTime := rec.Time.Format(timeFormat)
 		rec.Fields["when"] = formattedTime
 		return rec, nil
 	}
